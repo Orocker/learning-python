a46ff514eefc3c1aa4d02c2f1a821ca123271fa4
oumin <oumin@51ucar.cn>
2018-07-26 11:00:47 +0800

    add Slice.py & Iteration.py

--- Advanced-features/Iteration.py
+++ Advanced-features/Iteration.py
@@ -1,14 +1,4 @@
 # -*- coding: utf-8 -*-
-def trim(s):
-    if (s[:1] == ' '):
-        return trim(s[1:])
-    elif (s[-1:] == ' '):
-        return trim(s[:-1])
-    else:
-        return s
-
-# print(trim(' dada'))
-# print(trim(' dada1 '))
 
 dict = {'a':1,'b':2,'c':3}
 
