7ecdb34a3de8e772ed7a4504354a092764167aef
oumin <om@hao.xyz>
2018-07-27 14:50:54 +0800

    Update definition

--- Function/definition.py
+++ Function/definition.py
@@ -1,6 +1,24 @@
 # -*- coding: utf-8 -*-
 import math
 
+#定义一个返回绝对值的函数
+def my_abs(x):
+	if x > 0:
+		return x
+	else:
+		return -x
+
+#定义一个返回绝对值的函数，对传入的参数类型检查
+def abs1(x):
+	if not isinstance(x,(int,float)):
+		raise TypeError('Bad Operand Type') 
+	if x > 0:
+		return x
+	else:
+		return -x
+
+
+#从一个点移动到另一个点，给出坐标、位移和角度，计算出新的新的坐标（返回一个tuple）
 def move(x,y,step,angle=0):
     nx = x+step * math.cos(angle)
     ny = y+step * math.sin(angle)
