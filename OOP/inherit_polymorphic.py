7be8e09be4654390928fd17f13ec6b5e21de9a5a
oumin <om@hao.xyz>
2018-08-02 16:47:04 +0800

    inherit polymorphic

---
+++ OOP/inherit_polymorphic.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+
+
+class Animal(object):
+    def run(self):
+        print('Animal is running...')
+        
+#继承
+#当子类和父类都有相同的方法是，子类的方法会覆盖父类的方法
+class Dog(Animal):
+    def run(self):
+        print('Dog is running...')
+
+class Cat(Animal):
+    def run(self):
+        print('Cat is running...')
+
+def run_twice(animal):
+    animal.run()
+    animal.run()
+
+a = Animal()
+d = Dog()
+c = Cat()
+
+print('a is Animal?', isinstance(a, Animal))
+print('a is Dog?', isinstance(a, Dog))
+print('a is Cat?', isinstance(a, Cat))
+
+print('d is Animal?', isinstance(d, Animal))
+print('d is Dog?', isinstance(d, Dog))
+print('d is Cat?', isinstance(d, Cat))
+
+run_twice(c)
