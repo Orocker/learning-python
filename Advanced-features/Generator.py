f7880bce58ecb0c10e39723cfe30e27c0da0aa05
DomainRocker <om@hao.xyz>
2018-09-19 11:00:08 +0800

    update

--- Advanced-features/Generator.py
+++ Advanced-features/Generator.py
@@ -8,24 +8,24 @@ gener = (x for x in range(10))
 #获取Generator下一个返回值，获取完后，抛出StopIteration错误
 next(gener)
 
-#迭代Generator
+# 迭代Generator
 
 for n in gener:
 	print(n)
 
-#for循环无法实现的时候，定义函数实现，如斐波那契数列
+# for循环无法实现的时候，定义函数实现，如斐波那契数列
 
-def fib(max):
+def fib( max ):
 	n,a,b = 0,0,1
 	while n<max:
-		yield b  #。如果一个函数定义中包含yield关键字，这个函数就不再是一个普通函数，而是一个generator
+		yield b  # 如果一个函数定义中包含yield关键字，这个函数就不再是一个普通函数，而是一个generator
 		a,b =b,a+b
 		n = n+1
 	return 'done'
 		
-#Generator 的执行过程,遇到yield就会中断，下次继续执行，直到没有yield，再执行就会报错
+# Generator 的执行过程,遇到yield就会中断，下次继续执行，直到没有yield，再执行就会报错
 
-def odd():
+def odd ():
 	print('step1')
 	yield 1
 	print('step2')
@@ -33,7 +33,7 @@ def odd():
 	print('step3')
 	yield 5
 
-#Generator获取返回值，返回值必须捕获StopIteration错误，返回值包含在StopIteration的value中
+# Generator获取返回值，返回值必须捕获StopIteration错误，返回值包含在StopIteration的value中
 
 data = fib(10)
 
--- OOP/class_instance.py
+++ OOP/class_instance.py
@@ -1,16 +1,13 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-#定义类
 class Student(object):
-	"""docstring for Student"""
-	#类的特殊方法，在创建实例的时候，把属性绑定上去，self表示实例本身
-	#
-	def __init__(self,name,score):
-		self.name = name
-		self.score = score
 
-	#和普通的函数相比，在类中定义的函数只有一点不同，就是第一个参数永远是实例变量self，并且，调用时，不用传递该参数。除此之外，类的方法和普通函数没有什么区别
-	def print_score(self):
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+
+    def print_score(self):
         print('%s: %s' % (self.name, self.score))
 
     def get_grade(self):
@@ -21,11 +18,12 @@ class Student(object):
         else:
             return 'C'
 
-#类的实例
-bart = Student() #<__main__.Student object at 0x0034EDD0>
-Student  #<class '__main__.Student'>
+bart = Student('Bart Simpson', 59)
+lisa = Student('Lisa Simpson', 87)
 
-#类绑定属性
-s.name = 'lily'
-s.name #'lily'
-		
+print('bart.name =', bart.name)
+print('bart.score =', bart.score)
+bart.print_score()
+
+print('grade of Bart:', bart.get_grade())
+print('grade of Lisa:', lisa.get_grade())
